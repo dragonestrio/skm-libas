7431e63bf63e1d74053c46e780967a825678009c
Satrio Nugroho <satrion93@gmail.com>
2022-09-29 09:46:02 +0700

    modified:   database/seeders/UsersSeeder.php

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -16,21 +16,7 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         Users::factory(1)->create();
         // Users::factory(100)->create();
-=======
-        DB::table('users')->insert([
-            "id" => 1,
-            'phone' => "+628422323",
-            'name' => "Super Admin",
-            'email' => 'superadmin@gmail.com',
-            'level' => 'superadmin',
-            'gender' => 'L',
-            'address' => 'address',
-            'date_born' => '2000-11-11',
-            'password' => Hash::make('Password'),
-        ]);
->>>>>>> e6c86d4ae319f8a1a8779e8069d1f46dce83cea2
     }
 }
