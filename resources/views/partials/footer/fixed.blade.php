7e9a82bd24e83ee762eca3c800adfe6df7ce191b
dragonestrio <9897428+dragonestrio@users.noreply.github.com>
2022-10-06 17:25:26 +0700

    Delete resources directory

--- resources/views/partials/footer/fixed.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="container-fluid bg-primary fixed-bottom">
-    <div class="container pt-2">
-        <center>
-            <p>@2021 Copyright
-                <a href="" class="text-decoration-none btn-md text-warning" data-bs-toggle="modal" data-bs-target="#popup">SN</a>
-            </p>
-        </center>
-    </div>
-</footer>
