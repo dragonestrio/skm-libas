713ebbab9929c50b339ed3c3cb2807b9b2589803
dragonestrio <9897428+dragonestrio@users.noreply.github.com>
2022-10-07 09:28:00 +0700

    Delete storage directory

--- storage/framework/views/75b541767a724a2a3bad281efe76187d533789e8.php
+++
@@ -1,5 +0,0 @@
- 
- <script src="<?php echo e(url('assets/plugin/bootstrap-5.1.1-dist/js/bootstrap.min.js')); ?>"></script>
- 
- 
- <script src="<?php echo e(url('assets/js/main.js')); ?>"></script><?php /**PATH /Users/satrionugroho/Sites/localhost/laravela/resources/views/layouts/js.blade.php ENDPATH**/ ?>
